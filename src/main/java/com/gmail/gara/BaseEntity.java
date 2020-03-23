337c91b76a0f61473f457e9c302305d2e2186fb9
Sasha Gara <gara.aleksandr2@gmail.com>
2020-03-23 21:05:31 +0200

    Adding new class BaseEntity

---
+++ src/main/java/com/gmail/gara/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.gmail.gara;
+
+public class BaseEntity {
+    private Long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+   public boolean isNew() {
+        return(this.id == null);
+
+   }
+}
